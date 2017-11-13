26be743e48a2d39a1375338ffdf11747bf5af11e
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-11-13 15:53:10 -0500

    Add posts new component

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import REACT, { Component } from 'react';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>
+        PostsNew!
+      </div>
+    );
+  }
+}
+
+export default PostsNew
